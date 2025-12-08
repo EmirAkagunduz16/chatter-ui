0773edc351b21c218f8aca178890f7bca4b8ebce
EmirAkagunduz16 <akagunduzemir04@gmail.com>
2025-12-08 23:24:23 +0300

    upd

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,11 +1,18 @@
 import { Link } from "react-router-dom";
 import Auth from "./Auth";
 import { Link as MUILink } from "@mui/material";
+import { useLogin } from "../../hooks/useLogin";
 
 const Login = () => {
+  const { login, error } = useLogin();
+
   return (
-    <Auth submitLabel="Login" onSubmit={async () => {}}>
-      <Link to={"/signup"} style={{alignSelf: 'center'}}>
+    <Auth
+      submitLabel="Login"
+      onSubmit={(request) => login(request)}
+      error={error ? "Invalid credentials" : ""}
+    >
+      <Link to={"/signup"} style={{ alignSelf: "center" }}>
         <MUILink>Signup</MUILink>
       </Link>
     </Auth>
