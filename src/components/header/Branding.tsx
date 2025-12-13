3d48ce589200fb3f9b84906962e779b1614f6f19
EmirAkagunduz16 <akagunduzemir04@gmail.com>
2025-12-13 13:35:36 +0300

    upd

---
+++ src/components/header/Branding.tsx
@@ -0,0 +1,29 @@
+import { Typography } from "@mui/material";
+import ForumIcon from "@mui/icons-material/Forum";
+
+const Branding = () => {
+  return (
+    <>
+      <ForumIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+      <Typography
+        variant="h6"
+        noWrap
+        component="a"
+        href="#app-bar-with-responsive-menu"
+        sx={{
+          mr: 2,
+          display: { xs: "none", md: "flex" },
+          fontFamily: "monospace",
+          fontWeight: 700,
+          letterSpacing: ".3rem",
+          color: "inherit",
+          textDecoration: "none",
+        }}
+      >
+        CHATTER
+      </Typography>
+    </>
+  );
+};
+
+export default Branding;
--- src/constants/apollo-client.ts
+++ src/constants/apollo-client.ts
@@ -1,12 +1,37 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { CombinedGraphQLErrors } from "@apollo/client/errors";
+import { ErrorLink } from "@apollo/client/link/error";
+import { Observable } from "rxjs";
 import { API_URL } from "./urls";
+import excludedRoutes from "./excluded-routes";
+import router from "../components/Routes";
+
+const logoutLink = new ErrorLink(({ error, operation, forward }) => {
+  if (CombinedGraphQLErrors.is(error)) {
+    const firstError = error.errors[0];
+    if ((firstError.extensions?.originalError as any)?.statusCode === 401) {
+      if (!excludedRoutes.includes(window.location.pathname)) {
+        router.navigate("/login");
+      }
+      // Boş bir response döndür (data: null)
+      return new Observable((observer) => {
+        observer.next({ data: null });
+        observer.complete();
+      });
+    }
+  }
+  // Diğer hatalarda zinciri devam ettir
+  return forward ? forward(operation) : undefined;
+});
+
+const httpLink = new HttpLink({
+  uri: `${API_URL}/graphql`,
+  credentials: "include",
+});
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: `${API_URL}/graphql`,
-    credentials: "include",
-  }),
+  link: logoutLink.concat(httpLink),
 });
 
 export default client;
